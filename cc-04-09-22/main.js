1f244a744981b329c32def9061f1f60ffed681f7
biancaat <btalabe@gmail.com>
2022-04-09 15:41:10 -0400

    Coding challenge and solution on 04-09-22

---
+++ cc-04-09-22/main.js
@@ -0,0 +1,12 @@
+// 
+// Parameters:
+// R: Write a function to split a string and convert it into an array of words.
+// E: * "Robin Singh" ==> ["Robin", "Singh"]
+//    * "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+// P: write function that take a string as an argument, 
+//    turn that string into an array
+//    return the string array
+
+function strToArray (str){
+    return str.split(" ");
+}
