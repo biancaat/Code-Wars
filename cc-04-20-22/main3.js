dbf785af9b854c88fa5862d3fb460061c386869b
biancaat <btalabe@gmail.com>
2022-04-20 13:32:59 -0400

    Coding challenge and solution for 04-20-22

---
+++ cc-04-20-22/main3.js
@@ -0,0 +1,9 @@
+// P:
+// R: You need to double the integer and return it.
+// E:  doubleInteger(2), 4)
+// P: double integer, return value
+
+function doubleInteger(i) {
+    // i will be an integer. Double it and return it.
+    return i * 2;
+  }
