74c333142a870e1d90cb91053e4a7b592bd16a56
biancaat <btalabe@gmail.com>
2022-04-20 13:29:38 -0400

    Coding challenge and solution for 04-20-22

---
+++ cc-04-20-22/main2.js
@@ -0,0 +1,8 @@
+// P:
+// R: Implement a function which convert the given boolean value into its string representation.
+// E:  booleanToString(true, 'true')
+// P: create a function converts boolean value to a string
+
+function booleanToString(b){
+    return b.toString();
+  }
