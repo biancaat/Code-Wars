20dd86f9cd031ef20ad2c9c403fc2cd451739856
biancaat <btalabe@gmail.com>
2022-04-24 11:53:36 -0400

    Coding challenge and solution for 04-24-22

---
+++ cc-04-24-22/main.js
@@ -0,0 +1,8 @@
+// P:
+// R:Write a function which converts the input string to uppercase.
+// E: ('hello','HELLO)
+// P: pass arguument though function that turns a sting into all uppercase
+
+function makeUpperCase(str) {
+    return str.toUpperCase();
+   }
