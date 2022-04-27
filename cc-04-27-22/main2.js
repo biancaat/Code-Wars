6e378ae048098903de9e8a54d480ce5335293ca1
biancaat <btalabe@gmail.com>
2022-04-27 11:13:23 -0400

    Coding challenge and solution

---
+++ cc-04-27-22/main2.js
@@ -0,0 +1,12 @@
+// P:
+// R: Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages. Your task is to calculate how many blank pages do you need. If n < 0 or m < 0 return 0.
+// E: n= 5, m=5: 25, n=-5, m=5:  0
+// P:condition of if m or n is less than or eqaul to 0 return 0, else return n*m
+
+function paperwork(n, m) {
+  if (n <=0 || m<=0){
+      return 0
+  } else {
+      return n*M
+  }
+}
