467b6d10a8dd08ff2282dbe77c38c4f36f77c258
Bianca Talabe <btalabe@gmail.com>
2022-05-13 09:35:21 -0400

    Coding challenge and solution

---
+++ cc-05-14-22/main2.js
@@ -0,0 +1,8 @@
+// P: number
+// R: return the missing angle of the triangle
+// E: otherAngle(30, 60), 90
+// P: subtract a and b form 180
+
+function otherAngle(a, b) {
+    return 180 - a - b
+  }
