ace7e685217b481621071b1045a5962a86f66bea
Bianca Talabe <btalabe@gmail.com>
2022-05-14 09:32:16 -0400

    Coding challenge and solution

---
+++ cc-05-14-22/main.js
@@ -0,0 +1,8 @@
+// P: numbers
+// R: calculate volume of a cube
+// E: (getVolumeOfCuboid(1,2,2), 4)
+// P: multiply length, width and height
+
+function getVolumeOfCuboid (length, width,height){
+    return length*width*height
+}
