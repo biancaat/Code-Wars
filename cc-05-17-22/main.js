a865f8221c0f149dd1e0a312537e04bd467da3a4
Bianca Talabe <btalabe@gmail.com>
2022-05-17 17:19:37 -0400

    Coding challenge and solution

---
+++ cc-05-17-22/main.js
@@ -0,0 +1,8 @@
+// P: string
+// R: return true if string ends with ending 
+// E: solution('abc', 'bc') // returns true, solution('abc', 'd') // returns false
+// P: use turnary and return true if str ends with ending using endsWith() method
+
+function solution (str, ending){
+    return str.endsWith(ending)
+}
