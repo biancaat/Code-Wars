e247dfcea933f3ee77559ef22879a27d1a80da55
Bianca Talabe <btalabe@gmail.com>
2022-05-29 09:11:41 -0400

    coding challenge and solution

---
+++ cc-05-29-22/main.js
@@ -0,0 +1,8 @@
+// P: 2 numbers
+// R: number
+// E: breakChocolate(5, 5) , 24
+// P: return n * m -1, if any parameter = 0, return 0
+
+function breakChocolate(n,m) {
+    return n * m > 0 ? n*m - 1 : 0
+  }
