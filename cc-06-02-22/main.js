1ed2679e0a8e5b1304851106110031efd0e519d0
Bianca Talabe <btalabe@gmail.com>
2022-06-02 18:43:08 -0400

    Coding challenge and solution

---
+++ cc-06-02-22/main.js
@@ -0,0 +1,9 @@
+// P: 2 numbers
+// R: boolean
+// E: checkForFactor(10,2), true
+// P: turnary, if base % factor === 0 return true
+
+
+function checkForFactor (base, factor) {
+    return base % factor === 0 ? true : false
+  }
