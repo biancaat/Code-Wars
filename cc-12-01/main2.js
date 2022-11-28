3003f3dd204da7daa28a4867c3a6c8a7be28fd07
Bianca Talabe <btalabe@gmail.com>
2022-11-28 12:12:12 -0500

    Daily coding challenge

---
+++ cc-12-01/main2.js
@@ -0,0 +1,8 @@
+// P: number even or odd, can be negative or decimal
+// R: true if number even, false if not. decimaals are considered uneven
+// E: 2 => true, 4.6 => false, 5 => false
+// P:turnary if n less than 10 return string greater than 10 return other string
+
+function testEven(n) {
+    return n % 2 == 0 ? true : false
+}
