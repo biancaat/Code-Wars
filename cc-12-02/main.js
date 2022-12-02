7c71b5fe7ac706894b890bb89e2c4922596482ad
Bianca Talabe <btalabe@gmail.com>
2022-12-02 10:29:00 -0500

    Coding challenge for the day

---
+++ cc-12-02/main.js
@@ -0,0 +1,8 @@
+// P: number value that represents the nth even number we want returned, whole number, not negative, will not be 0
+// R: the even number in the nth position
+// E: 1 => 0, 3 => 4, 100 => 198
+// P: n x 2 -2 == nth even number 
+
+function nthEven(n){
+    return (n*2) -2
+  }
